1240cc6016ee2742c412a1d22b5b63ac4ade476c
vanderbruggem <mvdbrugge@hotmail.com>
2018-03-27 13:44:08 +1000

    Add project files.

---
+++ AutomationTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AutomationTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("GOV")]
+[assembly: AssemblyProduct("AutomationTest")]
+[assembly: AssemblyCopyright("Copyright © GOV 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5227d58e-dd74-46d1-9079-0537ffc50c01")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ AutomationTest/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace AutomationTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://bugzilla.mozilla.org/"); //this will open the web page
+            driver.Close(); // this will close the browser session
+            driver.Quit(); // this will stop the webdriver
+        }
+
+    }
+}
