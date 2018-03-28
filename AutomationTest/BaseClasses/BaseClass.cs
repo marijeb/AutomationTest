b16e2f0bc348bfa13e91eae4aff6678a918f5cad
vanderbruggem <mvdbrugge@hotmail.com>
2018-03-28 11:50:13 +1000

    Updates

---
+++ AutomationTest/BaseClasses/BaseClass.cs
@@ -0,0 +1,60 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.IE;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AutomationTest.Settings;
+using AutomationTest.Configuration;
+using AutomationTest.CustomException;
+
+namespace AutomationTest.BaseClasses
+{
+    public class BaseClass
+    {
+        private static IWebDriver GetFireFoxDriver()
+        {
+            IWebDriver driver = new FirefoxDriver();
+            return driver;
+        }
+
+        private static IWebDriver GetChromeDriver()
+        {
+            IWebDriver driver = new ChromeDriver();
+            return driver;
+
+        }
+
+        private static IWebDriver GetIEDriver()
+        {
+            IWebDriver driver = new InternetExplorerDriver();
+            return driver;
+        }
+
+        [AssemblyInitialize]
+        public static void InitWebdriver(TestContext tc)
+        {
+            ObjectRepository.Config = new AppConfigReader();
+
+            switch (ObjectRepository.Config.GetBrowser())
+            {
+                case BrowserType.FireFox:
+                    ObjectRepository.Driver = GetFireFoxDriver();
+                    break;
+                case BrowserType.Chrome:
+                    ObjectRepository.Driver = GetChromeDriver();
+                    break;
+                case BrowserType.IExplorer:
+                    ObjectRepository.Driver = GetIEDriver();
+                    break;
+                default:
+                    throw new NoSuitableDriverFound("Driver not found : " + ObjectRepository.Config.GetBrowser().ToString());
+            }
+        }
+    }
+}
+
---
+++ AutomationTest/Settings/ObjectRepository.cs
@@ -0,0 +1,16 @@
+﻿using AutomationTest.Interfaces;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomationTest.Settings
+{
+    public class ObjectRepository
+    {
+        public static IConfig Config { get; set; }
+        public static IWebDriver Driver { get; set; }
+        }
+}
