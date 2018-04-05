db5e00ae467742b0d592092dffd25a3c862874fa
vanderbruggem <mvdbrugge@hotmail.com>
2018-04-05 15:26:08 +1000

    DropDownList

---
+++ AutomationTest/ComponentHelper/DropDrownHelper.cs
@@ -0,0 +1,31 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AutomationTest.ComponentHelper
+{
+    public class DropDrownHelper
+    {
+        private static SelectElement select;
+
+        public static void SelectElement(By locator, int index)
+        {
+            select = new SelectElement(Helper.GetElement(locator));
+            select.SelectByIndex(index);
+        }
+
+        public static void SelectElement(By locator, string visibletext)
+        {
+            select = new SelectElement(Helper.GetElement(locator));
+            select.SelectByText(visibletext);
+        }
+        public static IList<string> GetAllItem(By locator)
+        {
+            select = new SelectElement(Helper.GetElement(locator));
+            return select.Options.Select((x) => x.Text).ToList();
+        }
+
+    }
+
+}
---
+++ AutomationTest/TestScript/DropDown/DropDownList.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using OpenQA.Selenium;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AutomationTest.ComponentHelper;
+using AutomationTest.Settings;
+using OpenQA.Selenium.Support.UI;
+using System.Collections.Generic;
+
+namespace AutomationTest.TestScript.DropDown
+{
+    //used different path as user has no admin rights and therefore cannot get to page reference in course
+    [TestClass]
+    public class DropDownList
+    {
+        [TestMethod]
+        public void TestList()
+        {
+            NavigationHelper.NavigateToUrl(ObjectRepository.Config.GetWebsite());
+            Helper.Click(By.LinkText("Search"));
+            Helper.Click(By.Id("tab_specific"));
+            //IWebElement element = ObjectRepository.Driver.FindElement(By.Id("bug_status"));
+            //SelectElement select = new SelectElement(element);
+            //select.SelectByIndex(0);
+            //select.SelectByValue("__closed__");
+            //select.SelectByText("All");
+            //Console.WriteLine("Selected value: {0}", select.SelectedOption.Text);
+            //IList<IWebElement> list = select.Options;
+            //foreach (IWebElement ele in list)
+            //{
+            //    Console.WriteLine("Value: {0}, Text: {1}", ele.GetAttribute("value"), ele.Text);
+            //}
+            DropDrownHelper.SelectElement(By.Id("bug_status"), 1);
+            DropDrownHelper.SelectElement(By.Id("bug_status"), "All");
+            foreach (string str in DropDrownHelper.GetAllItem(By.Id("bug_status")))
+            {
+                Console.WriteLine("Text: {0}", str);
+            }
+
+        }
+    }
+
+}
