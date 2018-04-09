5cf0974f0a9d77e456c73e5e4488f061ddb3442e
vanderbruggem <mvdbrugge@hotmail.com>
2018-04-09 11:21:52 +1000

    List All Elements on page

---
+++ AutomationTest/TestScript/FindElements/HandleElements.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using OpenQA.Selenium;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AutomationTest.ComponentHelper;
+using AutomationTest.Settings;
+using OpenQA.Selenium.Support.UI;
+using System.Collections.Generic;
+
+namespace AutomationTest.TestScript.FindElements
+{
+    [TestClass]
+    public class HandleElements
+    {
+        [TestMethod]
+        public void GetAllElements()
+        {
+            NavigationHelper.NavigateToUrl(ObjectRepository.Config.GetWebsite());
+            IReadOnlyCollection<IWebElement> elements =  ObjectRepository.Driver.FindElements(By.XPath("//input"));
+            IReadOnlyCollection<IWebElement> elements2 = ObjectRepository.Driver.FindElements(By.Id("123"));
+            foreach(var ele in elements)
+            {
+                Console.WriteLine("ID: {0}", ele.GetAttribute("name"));
+            }
+
+        }
+            
+
+    }
+}
